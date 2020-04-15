687b18fec766a04083f5784c04e22f3254eb453d
Timur Safin <tsafin@tarantool.org>
2020-04-15 15:33:08 +0300

    simple perl parser for queries bench results in bench.log

---
+++ report.pl
@@ -0,0 +1,20 @@
+my %Q, @Q;
+my $q = undef;
+while(<>) {
+  if (m[^queries/(.*).sql]) {
+    $q = $1;
+    unless (defined $Q{$q}) {
+      push(@Q, $q);
+      $Q{$q} = 'Q'.$q;
+    }
+  }
+  if (/^real\s+(\d+)m(\S+)s/) {
+    my $secs = ($1*60 + $2);
+    $Q{$q} .= ';'. $secs;
+  }
+}
+
+# foreach my $l (sort { $a <=> $b } keys %Q) {
+foreach my $l (@Q) {
+  print "$Q{$l}\n";
+}
