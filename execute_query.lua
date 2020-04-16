999e74bbdbc44449928a43af1cebeccb9e03edc4
Timur Safin <tsafin@tarantool.org>
2020-04-17 00:57:53 +0300

    restore box.cfg, reindent

--- execute_query.lua
+++ execute_query.lua
@@ -1,70 +1,61 @@
--- box.cfg{ listen = 3301, memtx_memory = 10 * 1024^3 }
+box.cfg{ listen = 3301, memtx_memory = 10 * 1024^3 }
+local socket = require 'socket'
 
 local function exec_query(cdata)
     print(cdata)
     return box.execute(cdata)
 end
 
---[[ 
-ok, error = exec_query [[
-        select
-	l_returnflag,
-	l_linestatus,
-	sum(l_quantity) as sum_qty,
-	sum(l_extendedprice) as sum_base_price,
-	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
-	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
-	avg(l_quantity) as avg_qty,
-	avg(l_extendedprice) as avg_price,
-	avg(l_discount) as avg_disc,
-	count(*) as count_order
-from
-	lineitem
-where
-	l_shipdate <= date('1998-12-01', '-71 days')
-group by
-	l_returnflag,
-	l_linestatus
-order by
-	l_returnflag,
-        l_linestatus;
-]]
---]]
+local function bench(func, iterations)
+    -- hopefully socket.gettime will provide us microsecond precision
+    local t = socket.gettime()
 
---[[
-if ok == nil then
-    print(error)
+    for i=1,iterations,1 do
+        func()
+    end
+    t = socket.gettime() - t
+    
+    -- ignore negligible timings
+    if t < (0.001*iterations) then
+        return nil
+    end
+
+    return t
 end
---]]
 
 local function flat_file_string(qname) 
-        local f = assert(io.open(qname, "rb"))
-        print (qname)
-        local query = {}
-        local line
-        while true do
-                line = f:read('*line')
-                if not line then break end
-                line = string.gsub(line, '%s+', ' ')
+    local f = assert(io.open(qname, "rb"))
+    -- print (qname)
+    local query = {}
+    local line
+    while true do
+        line = f:read('*line')
+        if not line then break end
+        line = string.gsub(line, '%s+', ' ')
 
-                -- skip empty lines or comments
-                if not string.match(line, '^%s*$') and
-                not string.match(line, '^%-%-.*$') then
-                
-                -- print(line)
-                table.insert(query, line)
-                end
-                if string.match(line, ';$') then break end
+        -- skip empty lines or comments
+        if not string.match(line, '^%s*$') and
+        not string.match(line, '^%-%-.*$') then
+        
+        -- print(line)
+        table.insert(query, line)
         end
+        if string.match(line, ';$') then break end
+    end
 
-        local flatten = table.concat(query, ' '):gsub('%s+', ' '):gsub(';', '')
-        return flatten
+    return table.concat(query, ' '):gsub('%s+', ' '):gsub(';', '')
 end
 
+local repeatitions = 4
 for q = 1,22,1 do
     local qname = string.format("queries/%s.sql", q)
-    print(flat_file_string(qname))
+    t_ = bench(
+            function() 
+                local query_line = flat_file_string(qname) -- qname captured
+                exec_query(query_line)
+            end, repeatitions)
+    print("Q"..q .. ';' .. (t_ and t_ / repeatitions or -1))
 end
 
 
--- os.exit(0)
+os.exit(0)
