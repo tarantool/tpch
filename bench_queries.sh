6317c49e95bd20f699f9013fb915390860617a2e
Oleg Piskunov <o.piskunov@tarantool.org>
2020-05-31 11:29:59 +0300

    addign sqlite Q22 for testing in CI

--- bench_queries.sh
+++ bench_queries.sh
@@ -8,8 +8,8 @@ function check_q {
 	)
 }
 
-# we need to execute all from 1 to 22 (except 13, 17, 20 and 22 in CI)
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 14 15 16 18 19 21; do
+# we need to execute all from 1 to 22 (except 13, 17 and 20 in CI)
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 14 15 16 18 19 21 22; do
 	check_q $i
 	check_q $i
 	check_q $i
